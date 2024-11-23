cc9b873b1de223481255e69419382dd38ff5545e
Wenny Yustalim <wennyyustalim@gmail.com>
2024-11-23 14:44:08 -0800

    Add mapbox integration

--- ruptor-ui/app/layout.tsx
+++ ruptor-ui/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
